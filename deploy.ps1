eeca0d3b15f472c6e66962eeea3380573ef2d483
Chad <867697+chadsterBAM@users.noreply.github.com>
2022-07-24 21:55:18 -0400

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -2,3 +2,5 @@ $date = Get-Date -Format "MM-dd-yyyy"
 $deploymentName = "ChadsWP"+"$date"
 
 New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName azure_admins_chad -TemplateFile .\main.bicep -TemplateParameterFile .\azuredeploy.parameters.json
+Write-Output "Creating Resources"
+Exit
